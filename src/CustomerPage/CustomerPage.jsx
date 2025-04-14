f0421bb0e2418895288f194dea1a2d6c2dc9c16c
Ruimeng Chen <rc4889@nyu.edu>
2025-04-14 18:13:37 -0400

    Add my new feature

--- src/CustomerPage/CustomerPage.jsx
+++ src/CustomerPage/CustomerPage.jsx
@@ -1,79 +1,218 @@
 import "./CustomerPage.css";
 
+// Import all images here
+import image190 from "/src/CustomerPage/assets/image-190.png";
+import image180 from "/src/CustomerPage/assets/image-180.png";
+import image200 from "/src/CustomerPage/assets/image-200.png";
+import image201 from "/src/CustomerPage/assets/image-201.png";
+import image210 from "/src/CustomerPage/assets/image-210.png";
+import image220 from "/src/CustomerPage/assets/image-220.png";
+import image230 from "/src/CustomerPage/assets/image-230.png";
+import image240 from "/src/CustomerPage/assets/image-240.png";
+import image250 from "/src/CustomerPage/assets/image-250.png";
+import image260 from "/src/CustomerPage/assets/image-260.png";
+import mg7617_10 from "/src/CustomerPage/assets/img-7617-10.png";
+import image280 from "/src/CustomerPage/assets/image-280.png";
+import image320 from "/src/CustomerPage/assets/image-320.png";
+import image40 from "/src/CustomerPage/assets/image-40.png";
+import vector2000 from "/src/CustomerPage/assets/vector-2000.svg";
+
 export const CustomerPage = ({ className, ...props }) => {
     return (
         <div className={"customer-page " + className}>
-            <div className="section">
+            <div className="list">
                 <div className="container">
-                    <img className="giphy-1" src="giphy-10.png" alt="Customer Image" />
-                    <div className="title">Welcome to Our Store Management</div>
-                    <div className="title2">Select a customer from the list below:</div>
+                    <div className="title">Select a customer from the customer list: </div>
+                </div>
+                <div className="list2">
+                    <div className="row">
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">😃 </div>
+                                <img className="image-19" src={image190} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Shen Zhou </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <img className="image-18" src={image180} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Kanye West </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <img className="image-20" src={image200} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Taylor Swift </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">👍 </div>
+                                <img className="image-202" src={image201} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">NotAlian </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">🎉 </div>
+                                <img className="image-21" src={image210} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Kimi </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">🌟 </div>
+                                <img className="image-22" src={image220} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Timi </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <img className="image-23" src={image230} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Timothée Chalamet </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">📞 </div>
+                                <img className="image-24" src={image240} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Anne Hathaway </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">🔑 </div>
+                                <img className="image-25" src={image250} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Mimi </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">💻 </div>
+                                <img className="image-26" src={image260} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Yu Liu </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">📆 </div>
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">dollar </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">📌 </div>
+                                <img className="img-7617-1" src={mg7617_10} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Fifteen </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">📋 </div>
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">FourEightSeven </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">📧 </div>
+                                <img className="image-28" src={image280} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Harry </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <img className="image-32" src={image320} />
+                            <div className="frame-427318906">
+                                <div className="title2">Customer 15 </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">📯 </div>
+                                <img className="image-4" src={image40} />
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">John Doe </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">📜 </div>
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Customer 17 </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">📇 </div>
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Customer 18 </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">📚 </div>
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Customer 19 </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <div className="frame">
+                                <div className="icon">🖊️ </div>
+                            </div>
+                            <div className="frame-427318906">
+                                <div className="title2">Customer 20 </div>
+                            </div>
+                        </div>
+                    </div>
+                    <img className="vector-200" src={vector2000} />
+                </div>
+                {/* <img className="img-8734-1" src="img-8734-10.png" /> */}
+                {/* (Nav bar removed) */}
+                <div className="section">
+                    <div className="container2">
+                        <div className="title4">Contact Us: buyaozhaowomen@store.com </div>
+                        <div className="title5">Copyright © 2025 Store Management </div>
+                    </div>
                 </div>
             </div>
-            <div className="form">
-                <div className="container2">
-                    <div className="title3">Customer List</div>
-                </div>
-                <div className="list">
-                    {/* Map over the customers */}
-                    <div className="row">
-                        <div className="input">
-                            <div className="title4">Customer 1: Alice Johnson</div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input">
-                            <div className="title4">Customer 2: Bob Lee</div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input">
-                            <div className="title4">Customer 3: Charlie Smith</div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input">
-                            <div className="title4">Customer 4: Diana Garcia</div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input">
-                            <div className="title4">Customer 5: Eve Kim</div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input">
-                            <div className="title4">Customer 6: Frank Wright</div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input">
-                            <div className="title4">Customer 7: Grace Lin</div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input">
-                            <div className="title4">Customer 8: Henry Zhao</div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input">
-                            <div className="title4">Customer 9: Isabel Chen</div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input">
-                            <div className="title4">Customer 10: Jackie Nguyen</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="button">
-                    <div className="primary">
-                        <div className="title6">Select Customer</div>
-                    </div>
-                </div>
-            </div>
-            <img className="vector-200" src="vector-2000.svg" alt="Vector Image" />
         </div>
     );
 };
+
