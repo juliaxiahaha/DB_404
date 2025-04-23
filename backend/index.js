ebd2629d9ace9fab0ce031bd85132645865aba1a
Heterohabilis <mutong221@gmail.com>
2025-04-22 22:05:55 -0400

    added updateProductReview endpoint

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,7 @@ import updateProduct    from './routes/updateProduct.js';
 import updateOrder       from './routes/updateOrder.js';
 import updateOrderDetail from './routes/updateOrderDetail.js';
 import updateShoppingCart from './routes/updateShoppingCart.js';
+import updateProductReview from './routes/updateProductReview.js';
 
 const DB_HOST='db-mysql-nyc3-77807-do-user-20581125-0.l.db.ondigitalocean.com'
 const DB_USER='doadmin'
@@ -124,6 +125,7 @@ app.use('/api/products', updateProduct(db));
 app.use('/api/orders', updateOrder(db));
 app.use('/api/orderDetails', updateOrderDetail(db));
 app.use('/api/shoppingCarts', updateShoppingCart(db));
+app.use('/api/productReviews', updateProductReview(db));
 
 
 app.listen(PORT, () => {
